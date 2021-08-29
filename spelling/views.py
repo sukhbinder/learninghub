8c206be0397bd09ae9c9df46e68fc8213d4a4e2a
sukhbinder <sukh@yahoo.com>
2021-08-29 06:48:19 +0000

    Adding voice only words

--- spelling/views.py
+++ spelling/views.py
@@ -9,21 +9,38 @@ from .forms import UploadFileForm
 import numpy as np
 # Create your views here.
 
+def get_sub_voice_chapter(line):
+    linelist = line.strip().split(',')
+    subject = linelist[0]
+    voiceonly = False
+    chapter = 0
+    if len(linelist) >1:
+        if isinstance(linelist[1], str):
+            if "true" in linelist[1].lower():
+                 voiceonly = True
+        elif isinstance(linelist[2], int):
+            chapter = int(linelist[2])
+    if len(linelist) > 2:
+        if isinstance(linelist[2], int):
+            chapter = int(linelist[2])
+    return subject, voiceonly, chapter
 
 def handle_uploaded_file(file):
     sub_dict = {sub.name: sub for sub in Subject.objects.all()}
     first_line = None
     for line in file:
-        line = str(line.decode("ascii")).strip()    
+        line = str(line.decode("ascii")).strip()
         if first_line is None:
-            first_line = line
+            first_line, voiceonly, chapter = get_sub_voice_chapter(line)
             assert first_line in sub_dict.keys(),  "{} Subject not listed in {}".format(
                 first_line, sub_dict.keys())
             continue
         try:
             question, answer = line.strip().split(",")
             w = Word(question=str(question), answer=str(
-                answer), subject=sub_dict[first_line])
+                answer), subject=sub_dict[first_line],
+                chapter=chapter, isvoiceonly=voiceonly
+                )
             w.save()
         except Exception as ex:
             print(ex)
